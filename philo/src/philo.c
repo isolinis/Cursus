b79dc4cde767373f1f3d350167caa06f0b30c4fc
isolinis <inaki.solinis@hotmail.com>
2022-01-28 16:00:30 +0100

    trying to solve munmap_chunk error

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -13,15 +13,21 @@
 #include "philo.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void	*ft_sit_up(void *params)
 {
 	t_philo	philo;
 	t_diner	*diner;
+	int	i;
 
 	diner = params;
+	diner->fork = (pthread_mutex_t *) malloc (diner->philos * sizeof(pthread_mutex_t));
 	philo = (t_philo){.tid = diner->tid, .time = 0,
 		.lm = 0, .dishes = diner->eat_nbr, .diner = diner};
+	i = 1;
+	while (i <= diner->philos)
+		pthread_mutex_init(&diner->fork[i++], NULL);
 	gettimeofday(&philo.sit, NULL);
 	gettimeofday(&philo.lastdish, NULL);
 	pthread_mutex_unlock(&diner->identify);
@@ -37,7 +43,6 @@ void	*ft_sit_up(void *params)
 			break ;
 		ft_thinking_corner(&philo);
 	}
-	pthread_mutex_destroy(&philo.diner->fork[philo.tid]);
 	return (NULL);
 }
 
@@ -67,26 +72,20 @@ void	ft_philo_leave(t_diner *diner)
 	i = 0;
 	while (i < diner->philos)
 	{
-		ret = pthread_join(diner->thread[i++], NULL);
+		ret = pthread_join(diner->thread[i], NULL);
 		if (ret != 0)
 			printf("Error joining the thread: %i\n", ret);
+		pthread_mutex_destroy(&diner->fork[i++]);
 	}
 	pthread_mutex_destroy(&diner->message);
 	pthread_mutex_destroy(&diner->identify);
 	free(diner->thread);
-	free(diner->fork);
 }
 
 void	ft_mise_en_place(t_diner *diner)
 {
-	int	i;
-
-	i = 0;
 	diner->leave = 0;
 	diner->thread = malloc (diner->philos * sizeof(pthread_t));
-	diner->fork = malloc (diner->philos * sizeof(pthread_mutex_t));
-	while (i < diner->philos)
-		pthread_mutex_init(&diner->fork[i++], NULL);
 	pthread_mutex_init(&diner->go, NULL);
 	pthread_mutex_init(&diner->message, NULL);
 	pthread_mutex_init(&diner->identify, NULL);
@@ -96,6 +95,7 @@ int	main(int argc, char **argv)
 {
 	t_diner	diner;
 
+	memset(&diner, 0, sizeof(t_diner));
 	if (!ft_arg_check(argc, argv, &diner))
 		return (1);
 	ft_mise_en_place(&diner);
