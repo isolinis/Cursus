2b6b1c50a325333c8becf343ee78ff8619d86347
Juan ignacio Solinis riobello <inaki.solinis@hotmail.com>
2022-05-11 20:53:18 +0200

    missing pointers to member functions

---
+++ C++/C01/ex05/Karen.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Karen.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsolinis <jsolinis@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/11 18:01:36 by jsolinis          #+#    #+#             */
+/*   Updated: 2022/05/11 20:52:05 by jsolinis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Karen.hpp"
+
+void	Karen::debug(void)
+{
+	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
+}
+void	Karen::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
+}
+void	Karen::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
+}
+void	Karen::error(void)
+{
+	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+}
+
+Karen::Karen(void)
+{
+}
+Karen::~Karen(void)
+{
+}
+
+void	Karen::complain(std::string)
+{
+		
+}
--- C++/C01/ex05/Karen.hpp
+++ C++/C01/ex05/Karen.hpp
@@ -6,7 +6,7 @@
 /*   By: jsolinis <jsolinis@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 23:28:28 by jsolinis          #+#    #+#             */
-/*   Updated: 2022/05/06 23:33:40 by jsolinis         ###   ########.fr       */
+/*   Updated: 2022/05/11 18:01:24 by jsolinis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,15 @@ private:
 
 	std::string level[4];
 
+	void	debug(void);
+	void	info(void);
+	void	warning(void);
+	void	error(void);
+
 public:
 	
 	Karen(void);
 	~Karen(void);
+
+	void	complain(std::string level);
 };
