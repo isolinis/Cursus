0ce45ceeef510e113fbbc10b7d0e499e3d4a7f1a
Iñaki Solinís Riobello <inaki.solinis@hotmail.com>
2022-11-15 17:51:37 +0100

    starting project

---
+++ ft_containers/includes/vector.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsolinis <jsolinis@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 18:55:01 by jsolinis          #+#    #+#             */
+/*   Updated: 2022/11/15 17:50:39 by jsolinis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+#include <iostream>
+#include <memory>
+#include "ft.hpp"
+
+using namespace ft;
+
+template <class T, class Alloc = std::allocator<T>>
+class Vector
+{
+
+};
+
+#endif
